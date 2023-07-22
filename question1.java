2f96773ea5181f52fa8e9190e4a02d6f0fd3d5d5
pavanikanijam <124563750+pavanikanijam@users.noreply.github.com>
2023-07-22 18:18:44 +0530

    question1.java

--- question1.java
+++ question1.java
@@ -3,31 +3,31 @@ import java.util.Arrays;
 public class solution1 {
 
 	    public static void main(String[] args) {
-	        int[] arrNum = {5, 3, 4, 2, 0, 8};
+	        int[] arr1 = {5, 3, 4, 2, 0, 8};
 
 	        try {
-	            calculateCumulativeMultiple(arrNum);
-	            System.out.println("Output: arrNum = " + Arrays.toString(arrNum));
+	            calc(arr1);
+	            System.out.println("Output: arrNum = " + Arrays.toString(arr1));
 	        } catch (ArithmeticException e) {
 	            System.out.println("Exception caught: " + e.getMessage());
 	        }
 	    }
 
-	    public static void calculateCumulativeMultiple(int[] arr) throws ArithmeticException {
-	        if (arr == null || arr.length == 0) {
+	    public static void calc(int[] arr2) throws ArithmeticException {
+	        if (arr2 == null || arr2.length == 0) {
 	            throw new IllegalArgumentException("Input array is null or empty.");
 	        }
 
-	        int cumulativeMultiple = 1;
-	        for (int i = 0; i < arr.length; i++) {
-	            if (arr[i] == 0) {
-	                for (int j = i + 1; j < arr.length; j++) {
-	                    arr[j] = 0;
+	        int Multiple = 1;
+	        for (int i = 0; i < arr2.length; i++) {
+	            if (arr2[i] == 0) {
+	                for (int j = i + 1; j < arr2.length; j++) {
+	                    arr2[j] = 0;
 	                }
 	                break;
 	            }
-	            cumulativeMultiple *= arr[i];
-	            arr[i] = cumulativeMultiple;
+	            Multiple *= arr2[i];
+	            arr2[i] =Multiple;
 	        }
 	    }
 	}
@@ -42,29 +42,37 @@ public class assignnment1 {
 	    	Scanner scanner = new Scanner(System.in);
 	   
 	    	System.out.print("Enter the number of elements in the array: ");
-	        int n = scanner.nextInt();
+	        int num1 = scanner.nextInt();
 
-	        int[] arrNum = new int[n];
+	        int[] arr1 = new int[num1];
 	        System.out.println("Enter the array elements:");
-	        for (int i = 0; i < n; i++) {
-	            arrNum[i] = scanner.nextInt();
+	        for (int i = 0; i < num1; i++) {
+	            arr1[i] = scanner.nextInt();
+	        }
+
+	        try {
+	            calc(arr1);
+	            System.out.println("Output: arrNum = " + Arrays.toString(arr1));
+	        } catch (ArithmeticException e) {
+	            System.out.println("Exception caught: " + e.getMessage());
 	        }
 	    }
-	    public static void calculateCumulativeMultiple(int[] arr) throws ArithmeticException {
-	        if (arr == null || arr.length == 0) {
+
+	    public static void calc(int[] arr2) throws ArithmeticException {
+	        if (arr2 == null || arr2.length == 0) {
 	            throw new IllegalArgumentException("Input array is null or empty.");
 	        }
 
-	        int cumulativeMultiple = 1;
-	        for (int i = 0; i < arr.length; i++) {
-	            if (arr[i] == 0) {
-	                for (int j = i + 1; j < arr.length; j++) {
-	                    arr[j] = 0;
+	        int Multiple = 1;
+	        for (int i = 0; i < arr2.length; i++) {
+	            if (arr2[i] == 0) {
+	                for (int j = i + 1; j < arr2.length; j++) {
+	                    arr2[j] = 0;
 	                }
 	                break;
 	            }
-	            cumulativeMultiple *= arr[i];
-	            arr[i] = cumulativeMultiple;
+	            Multiple *= arr2[i];
+	            arr2[i] =Multiple;
 	        }
 	    }
 	}
