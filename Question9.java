4c0ff68f5c93d9c8f10705116a03c148569f9fe4
pavanikanijam <124563750+pavanikanijam@users.noreply.github.com>
2023-07-22 20:16:56 +0530

    Question9.java

--- Question9.java
+++ Question9.java
@@ -1,69 +1,27 @@
 import java.util.*;
-
-class stu_dent1{
-   private int val;
-   private String fname;
-   private double cgpa;
-   public Student(int id, String fname, double cgpa) {
-      super();
-      this.val = id;
-      this.fname = fname;
-      this.cgpa = cgpa;
-   }
-   public int getVal() {
-      return token;
-   }
-   public String getFname() {
-      return fname;
-   }
-   public double getCgpa() {
-      return cgpa;
-   }
-}
-
-public class JavaPriorityQueue {
-
-  public static void main(String[] args) {
+public class test {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int totalEvents = Integer.parseInt(in.nextLine());
-        PriorityQueue<Student> list =
-                new PriorityQueue<>(new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                if(o1.getCgpa() != o2.getCgpa())
-                    return o1.getCgpa() < o2.getCgpa() ? 1 : -1;
-                else{
-                    if(!o1.getFname().equals(o2.getFname()))
-                        return o1.getFname().compareTo(o2.getFname());
-                    else
-                        return o1.getToken() < o2.getToken() ? 1 : -1;
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+        int n = in.nextInt();  
+        int m = in.nextInt();  
+        int val = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int n1 = in.nextInt();
+            deque.add(n1);
+            set.add(n1);
+            if (deque.size() == m) {
+                if (set.size() > val) {
+                    val = set.size();
                 }
+                int num = deque.remove();
+                if (!deque.contains(num)) set.remove(num);
             }
-        });
-
-        while (totalEvents > 0) {
-            String event = in.next();
-
-            if (event.equals("ENTER")) {
-                String name = in.next();
-                double gpa = in.nextDouble();
-                int id = in.nextInt();
-                list.add(new Student(id, name, gpa));
-            } else if (list.size() > 0 && event.equals("SERVED")) {
-                list.remove();
-            }
-
-            totalEvents--;
         }
+
         in.close();
-
-        if(list.size() == 0) {
-            System.out.println("EMPTY");
-            return;
-        }
-
-        while(list.size() > 0){
-            System.out.println(list.poll().getFname());
-        }
+        System.out.print(val);
     }
 }
